0a878bbd27189dae89c353060df8ecf517f2cc77
Ondrej Moris <omoris@redhat.com>
2015-02-27 18:50:08 +0100

    euler: temporal commit (solving Problem 36)

---
+++ euler-p0036.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"sandbox/euler"
+)
+
+func main() {
+	fmt.Printf("%d", euler.Number([]byte{1,0,0,1,0,0,1,0,0,1},2 ))
+}
--- euler/euler.go
+++ euler/euler.go
@@ -4,6 +4,8 @@ var primes []int = make([]int, 0)
 
 const max int = 1000000
 
+const MAX_DIGITS = 10
+
 func PrimalityTest(n int) bool {
 	for _, p := range primes { if n > p && n % p == 0 { return false } }
 	for i := 5; i*i <= n; i += 6 {
@@ -21,23 +23,23 @@ func Pow(a, b int) int {
 	return res
 }
 
-func Number(s []byte) int {
+func Number(s []byte, base int) int {
 	res := 0
 	for i := len(s) - 1; i >= 0; i-- {
-		res += int(s[len(s) - 1 - i]) * Pow(10, i)
+		res += int(s[len(s) - 1 - i]) * Pow(base, i)
 	}
 	return res
 }
 
-func Slice(n int) []byte {
+func Slice(n int, base int) []byte {
 	s := make([]byte, 0)
 	started := false
-	for i := 6; i >= 0; i-- {
-		p := Pow(10, i)
+	for i := MAX_DIGITS; i >= 0; i-- {
+		p := Pow(base, i)
 		v := byte(n / p)
 		n %= p
 		if v > 0 && !started { started = true }
-		if started { s = append(s, v) }		
+		if started { s = append(s, v) }
 	}
 	return s
 }
@@ -50,3 +52,6 @@ func Rotate(s []byte, l int) []byte {
 	return res
 }
 
+func Palindromic(n []byte) bool {
+	
+}
